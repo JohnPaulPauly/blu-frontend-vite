32c6ad814bc7de2a211e80111562d97a8f4e4ef1
01001100Trinh10 <luketrinh101@gmail.com>
2025-02-14 23:57:26 -0800

    Creating a basic profile that displays user information. Very unfinished will add an update button soon.

---
+++ src/views/UserProfile.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from 'axios';
+export default {
+  name: 'UserProfile',
+  data() {
+    return {
+      user: [],
+    };
+  },
+  async mounted() {
+    await this.fetchProfile();
+  },
+  methods: {
+    async fetchProfile() {
+      try {
+          const response = await axios.get("https://localhost:8080/users/fetch")
+          this.user = response.data;
+      }catch(error) {
+        console.error("Error fetching profile information", error);
+      }
+    },
+    async updateUser() {
+
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
