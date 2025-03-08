e8d1fb7d68d8c2b1b43e9a6bbaf15e5a6374e47f
Nicholas Tsimerekis <ntsimerekis@gmail.com>
2025-03-07 21:10:35 -0800

    stompy!

--- src/components/ScatterChart.vue
+++ src/components/ScatterChart.vue
@@ -25,6 +25,9 @@ import {
 } from 'chart.js'
 import { Scatter } from 'vue-chartjs'
 import * as chartConfig from '@/ChartConfig'
+import { Client } from '@stomp/stompjs';
+
+import axios from "axios";
 
 ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement)
 
@@ -38,9 +41,42 @@ onMounted(() => {
   setInterval(() => {
     //set data to random value
     data.value = chartConfig.randomData()
-
   }, 1000 )//1 second
+
+
+  //Get a uuid for a new path session
+  var uuid;
+
+  //You will receieve path ID here
+  axios.get("http://localhost:8080/paths/new").then((res) => {
+    uuid = res.data;
+    // console.log(uuid)
+    console.log(`/positions/${uuid}`);
+  })
+
+  // const stompClient = new StompJs.Client({
+  //   brokerURL: 'ws://localhost:8080/livepath'
+  // });
+
+  console.log(`/positions/${uuid}`);
+
+  //Recieve PATH data for that path ID
+  const client = new Client({
+    brokerURL: 'http://localhost:8080/ws/livepath',
+
+    onConnect: () => {
+      console.log("It's in. Oh my god it's in!")
+      client.subscribe(`/topic/[::1]`, message =>
+          //PLOT
+          console.log(`Received: ${message.body}`)
+      );
+    },
+  });
+  client.activate();
+
+
 })
+
 
 
 
