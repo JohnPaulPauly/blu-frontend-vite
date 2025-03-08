9378c54201e2bb19de6e850612389f48b55046c4
Raquel Hernandez <raquelhndz1@icloud.com>
2025-03-08 09:49:55 -0800

    about page start

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,13 @@
 // About Page
 <script setup>
-import { computed } from 'vue'
+import {computed, ref} from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import NavBar from "@/components/NavBar.vue";
+import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from '@heroicons/vue/24/outline'
+import bluLogo from "@/assets/images/BLUHorizontalLogo.svg";
+import bluLogoAlone from "@/assets/images/BluLogoAlone.svg";
+import {Dialog, DialogPanel} from "@headlessui/vue";
+import {Bars3Icon, XMarkIcon} from "@heroicons/vue/24/outline/index.js";
 
 const router = useRouter()
 const route = useRoute()
@@ -15,13 +20,119 @@ const search = computed({
     router.replace({ query: { search } })
   }
 })
+
+const features = [
+  {
+    name: 'Push to deploy',
+    description:
+        'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: 'SSL certificates',
+    description:
+        'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
+    icon: LockClosedIcon,
+  },
+  {
+    name: 'Simple queues',
+    description:
+        'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
+    icon: ArrowPathIcon,
+  },
+  {
+    name: 'Advanced security',
+    description:
+        'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
+    icon: FingerPrintIcon,
+  },
+]
+
+const navigation = [
+  { name: 'About', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Contact', href: '#' },
+]
+
+const mobileMenuOpen = ref(false)
+
 </script>
 
 <!--we will import the navbar for every page and add anything that is unique to the about page-->
 <template>
-  <NavBar/>
-  <h3 class = 'mt-4'>Under Construction!!! </h3>
-  <label>
-    Search: <input v-model.trim="search" maxlength="20">
-  </label>
+  <div class="bg-white">
+    <header class="absolute inset-x-0 top-0 z-50">
+      <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div class="flex lg:flex-1">
+          <a href="#" class="-m-1.5 p-1.5">
+            <span class="sr-only">BLU</span>
+            <img class="sm:h-18 h-16 w-auto " :src="bluLogo" alt="Blu Logo" />
+          </a>
+        </div>
+        <div class="flex lg:hidden">
+          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
+            <span class="sr-only">Open main menu</span>
+            <Bars3Icon class="size-8" aria-hidden="true" />
+          </button>
+        </div>
+        <div class="hidden lg:flex lg:gap-x-12">
+          <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-lg/6 roboto-bold text-gray-900 px-3 py-2 rounded-md transition duration-300 hover:bg-gray-100">{{ item.name }}</a>
+        </div>
+        <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="#" class="text-sm/6 font-semibold text-gray-900" v-on:click.prevent="LoginRoute">Log in <span aria-hidden="true">&rarr;</span></a>
+        </div>
+      </nav>
+      <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+        <div class="fixed inset-0 z-50" />
+        <DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div class="flex items-center justify-between">
+            <a href="#" class="-m-1.5 p-1.5">
+              <span class="sr-only">Your Company</span>
+              <img class="h-10 w-auto" :src="bluLogoAlone" alt="Blu Logo"  />
+            </a>
+            <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+              <span class="sr-only">Close menu</span>
+              <XMarkIcon class="size-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div class="mt-6 flow-root">
+            <div class="-my-6 divide-y divide-gray-500/10">
+              <div class="space-y-2 py-6">
+                <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 roboto-semibold text-gray-900 hover:bg-gray-50">{{ item.name }}</a>
+              </div>
+              <div class="py-6">
+                <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 roboto-bold text-gray-900 hover:bg-gray-50" v-on:click.prevent="LoginRoute">Log in</a>
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
+  </div>
+
+    <div class="bg-white py-24 sm:py-32">
+      <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto max-w-4xl lg:text-center">
+          <h2 class="sm:text-8xl  text-6xl roboto-bold text-blu800">About BLU</h2>
+          <p class="mt-2 text-2xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-3xl lg:text-balance">Innovating Indoor Tracking with Bluetooth Low Energy
+          </p>
+          <p class="mt-6 text-lg/6 roboto-semibold text-gray-500">At <span class ="text-blu800 roboto-bold">BLU</span>, we’re a team of engineers, innovators, and problem-solvers passionate about redefining location tracking. We built BLU as an affordable, scalable, and secure tracking solution powered by Bluetooth Low Energy and advanced trilateration algorithms.</p>
+        </div>
+        <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+          <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+            <div v-for="feature in features" :key="feature.name" class="relative pl-16">
+              <dt class="text-base/7 font-semibold text-gray-900">
+                <div class="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-indigo-600">
+                  <component :is="feature.icon" class="size-6 text-white" aria-hidden="true" />
+                </div>
+                {{ feature.name }}
+              </dt>
+              <dd class="mt-2 text-base/7 text-gray-600">{{ feature.description }}</dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+    </div>
+
+
 </template>
