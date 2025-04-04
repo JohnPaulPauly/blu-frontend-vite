c29321e9e2515add3ce03c16131e750635ccdc8f
Raquel Hernandez <raquelhndz1@icloud.com>
2025-04-03 23:03:43 -0700

    Safety commit

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,6 +1,6 @@
 // This component is a resuable component tha will be a search bar we can use to search through users , paths
 ip addresses etc. It will be created and tested first using a local example of the types of JSON data the search would
-receve. it can then be changed to  hook up to a backend in the proper form. We will atempt to impelement a fuxxy search as well.
+receve. it can then be changed to  hook up to a backend in the proper form. We will atempt to impelement a fuzzyy search as well.
 
 <script setup>
 
